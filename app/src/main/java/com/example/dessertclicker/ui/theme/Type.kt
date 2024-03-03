53afbf255d258c2dd38423ad9182efe16e655eef
Raul <rahaiduc@unirioja.es>
2024-03-03 20:51:57 +0100

    dessert clicker

---
+++ app/src/main/java/com/example/dessertclicker/ui/theme/Type.kt
@@ -0,0 +1,26 @@
+package com.example.dessertclicker.ui.theme
+
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    titleMedium = TextStyle(
+        color = Color(0xFF008577),
+        fontSize = 33.sp,
+    ),
+    titleLarge = TextStyle(
+        color =  Color(0x99000000),
+        fontSize = 20.sp
+    )
+)
